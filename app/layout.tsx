5a19d35a73d5136a7f78db139173b65a4d5373c1
José Bieco <jo_bieco@hotmail.com>
2025-05-09 18:20:30 -0300

    githubPages

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,26 +2,15 @@
 // app/layout.tsx
 import "./globals.css";
 import { ReactNode } from "react";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { FontSizeProvider } from "./context/FontSizeContext";
-import { theme } from "./utils/theme";
-// import FontSizeControls from "./components/FontSizeControls";
-// export const metadata = {
-//   title: "Seu App",
-//   description: "Descrição do seu app",
-// };
+import { CssBaseline } from "@mui/material";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-br">
       <body style={{ padding: 15 }}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <FontSizeProvider>
-            {/* <FontSizeControls /> */}
-            {children}
-          </FontSizeProvider>
-        </ThemeProvider>
+        <CssBaseline />
+
+        {children}
       </body>
     </html>
   );
