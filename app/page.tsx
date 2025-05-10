28394fca7ecc8fe14e2ddc674097544fda8d7e1c
José Bieco <jo_bieco@hotmail.com>
2025-05-10 14:02:50 -0300

    feat(app/page.tsx): adição de verificações para ir para próximos steps em alguns dos processos, juntamente com a adição de alertas para identificar que existe um problema.

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,10 @@ import Selecao from "./components/Selecao";
 import Confirmacao from "./components/Confirmacao";
 import Download from "./components/Download";
 import Avaliacao from "./components/Avaliacao";
+import { useTurmas } from "./context/TurmasContext";
+import { useAlertsContext } from "./context/AlertasContext";
+import { TurmaData } from "./types";
+import { useAvaliacao } from "./context/AvaliacaoContext";
 // import CheckIcon from "@mui/icons-material/Check";
 
 // Passos do processo
@@ -36,17 +40,76 @@ const steps = [
 //   );
 // };
 
+/**
+ * Retorna se o usuário poderá seguir para o step após a Confirmação.
+ * @param turmas Turmas Selecionadas
+ * @returns {boolean} ´True´ caso possa avançar, `False` caso contrário.
+ */
+function avancaStepConfirmacao(turmas: TurmaData[]): boolean {
+  const prioridadesDefinidas = new Set<number>();
+  /**
+   * Verifica se todas as turmas apresentam prioridades definidas.
+   */
+  for (const turma of turmas) {
+    if (turma.prioridade === undefined || turma.prioridade === 0) {
+      return false;
+    }
+    prioridadesDefinidas.add(turma.prioridade);
+  }
+
+  /**
+   * Verifica se as prioridades definidas são diferentes.
+   * Caso o Set (que armazena apenas valores diferentes) tiver se tamanho menor que a quantidade total
+   * de turmas, isso quer dizer que algum valor foi repetido.
+   */
+  if (prioridadesDefinidas.size !== turmas.length) {
+    return false;
+  }
+
+  return true;
+}
+
 const StepperFlow = () => {
   const [activeStep, setActiveStep] = useState(0);
   const [completed, setCompleted] = useState<{ [k: number]: boolean }>({});
+
+  const { selectedTurmas } = useTurmas();
+  const { addAlerta } = useAlertsContext();
+  const { avaliacao } = useAvaliacao();
 
   const totalSteps = () => steps.length;
 
   const isLastStep = () => activeStep === totalSteps() - 1;
 
   const handleNext = () => {
-    handleComplete();
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    /**
+     * Não pode haver continuidade do processo caso nas etapas de Seleção e Confirmação não exista nenhuma Turma selecionada.
+     * Deve ser barrado em ambos os casos.
+     */
+    if ([1, 2].includes(activeStep) && selectedTurmas.size === 0) {
+      addAlerta(
+        "Selecione ao menos uma Turma para que seja possível continuar.",
+        "error",
+        6
+      );
+      /**
+       * Para ter continuídade no processo de confirmação, nenhuma Turma pode apresentar Prioridade 0 ou repetida.
+       */
+    } else if (
+      activeStep === 2 &&
+      !avancaStepConfirmacao(selectedTurmas.values().toArray())
+    ) {
+      addAlerta(
+        "Todas as Turmas devem apresentar Prioridades diferetes de 0 e diferetes entre si.",
+        "error",
+        6
+      );
+    } else if (activeStep === 3 && !avaliacao.nota) {
+      addAlerta("Uma Nota deve ser selecionada.", "error", 6);
+    } else {
+      handleComplete();
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    }
   };
 
   const handleBack = () => {
@@ -67,10 +130,10 @@ const StepperFlow = () => {
     setCompleted(newCompleted);
   };
 
-  const handleReset = () => {
-    setActiveStep(0);
-    setCompleted({});
-  };
+  // const handleReset = () => {
+  //   setActiveStep(0);
+  //   setCompleted({});
+  // };
 
   const getStepContent = (step: number) => {
     switch (step) {
